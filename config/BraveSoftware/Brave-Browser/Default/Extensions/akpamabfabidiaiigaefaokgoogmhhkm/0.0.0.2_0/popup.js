41e090376380a8239e3438d41f485bebdee0e64f
unklebonehead <unklebonehead@nixnet.email>
2022-04-12 19:22:32 -0400

    update

--- config/BraveSoftware/Brave-Browser/Default/Extensions/akpamabfabidiaiigaefaokgoogmhhkm/0.0.0.2_0/popup.js
+++
@@ -1,7 +0,0 @@
-// popup.js
-
-chrome.runtime.sendMessage({greeting: "getInfo"}, function(response) {
-  document.querySelector('#accountname').value = response.accountname
-  document.querySelector('#hivepower').value = response.hivepower
-  document.querySelector('#fishname').value = response.fishname
-});
